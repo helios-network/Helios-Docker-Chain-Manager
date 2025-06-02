ad8aba2b7b581a34eb985199153b20caf2871fa7
jguyet <jguyet.weoohh@gmail.com>
2025-06-02 17:48:19 +0200

    0.0.15

--- release.sh
+++ release.sh
@@ -1,1 +1,1 @@
-docker buildx build --build-context clone=../ -t docker-helios-nodemanager:v0.0.1 -f ./Dockerfile-local-repositories .
+docker buildx build --platform linux/amd64 --build-context clone=../ --label version=v0.0.15 -t docker-helios-nodemanager:v0.0.15 -f ./Dockerfile-local-repositories .
