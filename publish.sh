22856faf0f201c1751537eda3aefb0cbc85b4b22
jguyet <jguyet.weoohh@gmail.com>
2025-06-02 17:48:19 +0200

    0.0.15

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-docker tag docker-helios-nodemanager:v0.0.1 heliosfoundation/docker-helios-nodemanager:v0.0.1
-docker push heliosfoundation/docker-helios-nodemanager:v0.0.1
-docker tag docker-helios-nodemanager:v0.0.1 heliosfoundation/docker-helios-nodemanager:latest
+docker tag docker-helios-nodemanager:v0.0.14 heliosfoundation/docker-helios-nodemanager:v0.0.14
+docker push heliosfoundation/docker-helios-nodemanager:v0.0.14
+docker tag docker-helios-nodemanager:v0.0.14 heliosfoundation/docker-helios-nodemanager:latest
 docker push heliosfoundation/docker-helios-nodemanager:latest
