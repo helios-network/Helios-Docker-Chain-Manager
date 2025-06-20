31eb7dd9497797498087a6f09b2cb6f393a249b9
jguyet <jguyet.weoohh@gmail.com>
2025-06-20 10:55:31 +0200

    disable possible double run

--- exposition/POST-run-miner-node.js
+++ exposition/POST-run-miner-node.js
@@ -2,8 +2,12 @@ const { runMinerNode } = require('../application/run-miner-node');
 
 const runMinerNodeRoute = (app, environement) => {
     app.post('/run-miner-node', async (req, res) => {
-        await runMinerNode(app, environement);
-        res.send('');
+
+        const status = await app.node.status();
+        if (status == '0') {
+            await runMinerNode(app, environement);
+        }
+        res.send(status == '0' ? false : true);
     });
 };
 
