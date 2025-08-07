7cd90a3e4080d0d5635aabcb6428ef703b402062
jguyet <jguyet.weoohh@gmail.com>
2025-08-07 22:16:00 +0200

    fix error of ethstats client

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-version=v0.0.119
+version=v0.0.122
 docker tag docker-helios-nodemanager:$version heliosfoundation/docker-helios-nodemanager:$version
 docker push heliosfoundation/docker-helios-nodemanager:$version
 docker tag docker-helios-nodemanager:$version heliosfoundation/docker-helios-nodemanager:latest
