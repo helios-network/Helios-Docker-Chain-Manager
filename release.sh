1cf39a79ff94b5e889973f2801963a65f999a399
jguyet <jguyet.weoohh@gmail.com>
2025-08-31 14:02:43 +0200

    0.0.154

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
-version=v0.0.137
+version=v0.0.154
 docker buildx build --platform linux/amd64 --build-context clone=../ --label version=$version -t docker-helios-nodemanager:$version -f ./Dockerfile-local-repositories .
